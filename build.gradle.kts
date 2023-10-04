144e4c8bca04abc9b8ee7f07c73ea3248b493bf1
Thomas Krut <thomas.k.krut@gmail.com>
2023-10-04 12:16:15 +0200

    jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.1.4"
     id("io.spring.dependency-management") version "1.1.3"
+    id("jacoco")
 }
 
 group = "com.example"
@@ -69,6 +70,16 @@ val integrationTest = task<Test>("integrationTest") {
 tasks {
     bootJar {
         archiveFileName.set("${archiveBaseName.get()}.${archiveExtension.get()}")
+    }
+}
+
+tasks {
+    jacocoTestReport {
+        dependsOn(build)
+        reports {
+            xml.required.set(true)
+            csv.required.set(true)
+        }
     }
 }
 
